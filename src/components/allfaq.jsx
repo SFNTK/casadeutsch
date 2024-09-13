e782f14221f776b67f01aad72a998e193207d54d
SFNTK <153030492+SFNTK@users.noreply.github.com>
2024-09-13 16:44:06 +0100

    Update allfaq.jsx

--- src/components/allfaq.jsx
+++ src/components/allfaq.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './allfaq.css'
 const faqData = [
-    { question: "la question 1", answer: "la réponse" },
-    { question: "la question 2", answer: "la réponse" },
-    { question: "la question 3", answer: "la réponse" }
+    { question: "Quels cours d'allemand proposez-vous ?", answer: "Nous proposons différents cours d'allemand, y compris des cours pour débutants (A1-A2), des cours pour niveaux intermédiaires (B1-B2) et des cours de préparation aux examens" },
+    { question: "Proposez-vous des cours adaptés aux entreprises ou aux professionnels ?", answer: "Oui, nous proposons des cours d'allemand sur mesure pour les entreprises et les professionnels, axés sur le vocabulaire et les compétences spécifiques au milieu professionnel" },
+    { question: "Combien de temps faut-il pour apprendre l'allemand ?", answer: "La durée dépend de votre niveau de départ et de vos objectifs d'apprentissage. En général, les débutants ont besoin d'environ 6 mois pour acquérir les bases de la langue." }
   ];
   
   const FAQItem = ({ question, answer }) => {
