389144b59235686292e399c9386fa8fc889514d1
SFNTK <153030492+SFNTK@users.noreply.github.com>
2024-09-26 14:24:34 +0100

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
-import "./about.css"
+import React ,{ useEffect }from 'react';
+import "./about.css";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 const About = () => {
+       useEffect(()=>{
+        const tls211 = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#about",
+                start: "top 80%",
+                end: "top -50%",
+               
+
+            }
+        });
+        tls211.fromTo("#about img",{opacity:.8,x:-20},{
+            opacity:1,x:1,duration:.6,ease:"power1.inOut"
+        }).fromTo("#abouttxt",{ opacity:0, transform: "scale(.9, .9)"},{
+            transform:"scale(1,1)",duration:.8,ease:"power1.inOut",opacity:1
+        })
+    },[])
     return (
         <div id='about'>
             <img src='./owner.jpg'/>
